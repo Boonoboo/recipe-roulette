51fbb6a522ad87fac410aad694a9752d0f0c06a0
Mathias Enggrob Boon <moon@netlight.com>
2022-12-05 22:54:39 +0100

    Implemented simple recipe picker

---
+++ lib/getApiToken.ts
@@ -0,0 +1,20 @@
+// Makes a request to the HelloFresh website to get a bearer token,
+// then returns it.
+export const getApiToken = async () => {
+  const res = await fetch(
+    "https://www.hellofresh.com/gw/auth/token?client_id=senf&grant_type=client_credentials&scope=public&locale=en-US&country=us",
+    {
+      method: "POST",
+    }
+  );
+  const data: GetTokenResponse = await res.json();
+  return data.access_token;
+};
+
+type GetTokenResponse = {
+  access_token: string;
+  expires_in: number;
+  issued_at: number;
+  scope: string;
+  token_type: string;
+};
---
+++ lib/getRecipe.ts
@@ -0,0 +1,54 @@
+import { getApiToken } from "./getApiToken";
+
+/**
+ * Gets a random recipe from the HelloFresh API
+ */
+export const getRecipe = async () => {
+  const token = await getApiToken();
+  const res = await fetch(
+    `https://www.hellofresh.com/gw/api/recipes/search/suggestions?country=US&locale=en-US&take=100`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const data: CMSResponse = await res.json();
+  const items = data.items.flatMap((item) => item.items);
+  const randomItem = items[Math.floor(Math.random() * items.length)];
+
+  const recipe = await fetch(
+    `https://www.hellofresh.com/gw/api/recipes/${randomItem.recipeId}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  const recipeData: unknown = await recipe.json();
+  return recipeData;
+};
+
+type CMSResponse = {
+  count: number;
+  items: ItemGroup[];
+  skip: number;
+  take: number;
+  total: number | null;
+};
+
+type ItemGroup = {
+  group: string;
+  group_title: string;
+  items: RecipeSearchResult[];
+};
+
+type RecipeSearchResult = {
+  headline: string;
+  image: string;
+  recipeId: string;
+  slug: string;
+  title: string;
+};
